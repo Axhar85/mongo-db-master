0cbfc43de6dd3b611ef6a02870feee10fb6f4dd4
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-09 20:16:36 +0200

    passed data and connected with mongo

--- test.js
+++ test.js
@@ -4,9 +4,17 @@ const connect = () => {
     return mongoose.connect('mongodb://localhost:27017/whatever')
 }
 
+// creating scheems
 const student = new mongoose.Schema ( {
-    firstName = String
-})
+    firstName : String
+});
 
 const Student = mongoose.model('student', student)
 
+//connecting with db
+connect()
+.then(async connection => {
+    const student = await Student.create({firstName: 'Tim'})
+    console.log(student)
+} )
+.catch(e => console.error(e))
